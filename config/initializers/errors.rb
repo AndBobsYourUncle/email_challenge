cfdd59d6320a5692c8d2d82102554d87654d8aca
Nicholas Page <npage85@gmail.com>
2018-04-19 23:26:53 -0700

    Replace with postgres for Heroku

--- config/initializers/errors.rb
+++ config/initializers/errors.rb
@@ -5,7 +5,7 @@ SMTP_SERVER_ERRORS = [
   Net::SMTPAuthenticationError,
   Net::SMTPServerBusy,
   Net::SMTPUnknownError,
-  TimeoutError
+  Timeout::Error
 ].freeze
 
 SMTP_CLIENT_ERRORS = [Net::SMTPFatalError, Net::SMTPSyntaxError].freeze
